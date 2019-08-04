ddfcfe4b12376ae686380053123d9b3641697abc
Juan Quintana <juan.armaguedon@gmail.com>
2019-08-04 13:35:03 +0200

    add plot module with a first histogram plotter function

---
+++ utils/TODO.md
@@ -0,0 +1,6 @@
+# To Do:
+
+- [] reader: reader iterativo para un conjunto de csv de una misma carpeta.
+- [] loader: modificar la posibilidad de guardar ficheros comprimidos.
+- [] timer: traer el util Timer() del respositorio DStools mejorando su accesibilidad.
+- [] plot.histogram: re-thinking structura and rename function names, etc.
---
+++ utils/plot/histogram.py
@@ -0,0 +1,11 @@
+## plot histogram from df
+def plot_hist_df(df, title, cumulative = False):
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    plt.figure(figsize=(8,4))
+    if cumulative: g = sns.distplot(df,hist_kws=dict(cumulative=True),kde_kws=dict(cumulative=True))
+    else: g = sns.distplot(df, bins = 100)
+    g.set_title(title, fontsize=18)
+    g.set_xlabel("x")
+    g.set_ylabel("Probability", fontsize=15)
+    plt.show()
